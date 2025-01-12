9fa94d238580251be76051e5281f6e87c64cf26b
pavv3l <pavv3l@gmail.com>
2025-01-12 22:06:46 +0100

    changes

--- main.c
+++ main.c
@@ -5,26 +5,43 @@
 #include "hardware/clocks.h"
 #include "parallel_16.pio.h"
 
-#define COLOR(r8, g8, b8)                  \
-    ((uint16_t)(((b8) >> 3) & 0x1FU) << 11 | \
-    (uint16_t)(((g8) >> 2) & 0x3FU) << 5 | (uint16_t)(((r8) >> 3) & 0x1FU))
+#define D0 6
+#define D15 21
+#define BITSIZE 16
+#define WRX 22 // LCD mcu read bytres on rising edge
+#define RDX // when only writing operations, always HIGH
+#define CSX 23// LOW for write command code, HIGH for parameter or display data
 
+#define COLOR(r8, g8, b8)                    \
+    ((uint16_t)(((b8) >> 3) & 0x1FU) << 11 | \
+     (uint16_t)(((g8) >> 2) & 0x3FU) << 5 | (uint16_t)(((r8) >> 3) & 0x1FU))
+
+void initGpio()
+{
+    gpio_init(WRX);
+    gpio_set_dir(WRX, GPIO_OUT);
+    gpio_init(CSX);
+    gpio_set_dir(CSX, GPIO_OUT);
+    for(int i = D0; i <= D15; i++)
+    {
+        gpio_init(i);
+        gpio_set_dir(i, GPIO_OUT);
+    }
+}
 
 int main()
 {
     stdio_init_all();
 
     PIO pio = pio0;
-    uint offset = pio_add_program(pio, &parallel_lcd_program
-    
-    );
+    uint offset = pio_add_program(pio, &parallel_lcd_program);
     printf("Loaded program at %d\n", offset);
 
     printf("System Clock Frequency is %d Hz\n", clock_get_hz(clk_sys));
     printf("USB Clock Frequency is %d Hz\n", clock_get_hz(clk_usb));
-    // For more examples of clocks use see https://github.com/raspberrypi/pico-examples/tree/master/clocks
 
-    while (true) {
+    while (true)
+    {
         printf("Hello, world!\n");
         sleep_ms(1000);
     }
